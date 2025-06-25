5d3f53c3986fa504ff9b85c6656b65eeefda9b6b
danielea93 <angelonedaniele@outlook.it>
2025-06-25 12:04:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -160,16 +160,14 @@ async def handle_alexa_request(request: Request):
                                 "height": "80vh",
                                 "width": "100%",
                                 "data": "${payload.list}",
-                                "items": [
-                                    {
-                                        "type": "Text",
-                                        "text": "${data.text}",
-                                        "style": "textStylePrimary2",
-                                        "paddingTop": 10,
-                                        "paddingBottom": 10,
-                                        "fontSize": "20dp"
-                                    }
-                                ]
+                                "item": {
+                                    "type": "Text",
+                                    "text": "${data.text}",
+                                    "style": "textStylePrimary2",
+                                    "paddingTop": 10,
+                                    "paddingBottom": 10,
+                                    "fontSize": "20dp"
+                                }
                             }
                         ],
                         "padding": 20
